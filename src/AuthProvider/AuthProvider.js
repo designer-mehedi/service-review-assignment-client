26bab25038f62e3bf555e2366c0bb9241bb87f2c
designermehedi <mehedihasan80912@gmail.com>
2022-11-13 01:09:50 +0600

    web app updated

--- src/AuthProvider/AuthProvider.js
+++ src/AuthProvider/AuthProvider.js
@@ -7,12 +7,15 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null); 
+    const [loading, setLoading] = useState(true); 
     
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const loginUser = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password) 
     }
 
@@ -20,6 +23,7 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log(currentUser); 
             setUser(currentUser); 
+            setLoading(false); 
         });
         return () => unsubscribe; 
     }, [])
@@ -27,7 +31,8 @@ const AuthProvider = ({children}) => {
     const authInfo = {
 		user,
 		createUser,
-		loginUser
+		loginUser,
+        loading
 	};
     return (
         <div>
