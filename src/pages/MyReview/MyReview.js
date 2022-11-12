b7ed3399dc3bc631f84c8342f98f0094f7dc33fb
designermehedi <mehedihasan80912@gmail.com>
2022-11-12 23:55:09 +0600

    Review detailse updated

--- src/pages/MyReview/MyReview.js
+++ src/pages/MyReview/MyReview.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import ReviewRow from "./ReviewRow";
 
 const MyReview = () => {
     return (
         <div>
-            
+           <div className="overflow-x-auto w-full max-w-screen-xl mx-auto">
+            <table className="table w-full">
+            <thead>
+                <tr>
+                <th>
+                    <label>
+                    <input type="checkbox" className="checkbox" />
+                    </label>
+                </th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+                <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <ReviewRow/>
+            </tbody>
+            </table>
+            </div> 
         </div>
     );
 };
